b368ddf14b5b39a680cbfa434e94cb548d3af162
Josh F <jjjoshua287@gmail.com>
2026-07-06 13:24:44 -0400

    x86_64: add setup TSS function
    - add macro to call function that writes a GDT entry.
    - add macro to call function that writes an IDT entry.

--- arch/x86_64/include/asm/desc.h
+++ arch/x86_64/include/asm/desc.h
@@ -3,6 +3,8 @@
 
 #include <asm/desc_defs.h>
 #include <asm/segment.h>
+#include <asm/processor.h>
+
 #include <emerald/string.h>
 
 static inline void pack_gate(gate_desc *gate, unsigned type, unsigned long func, 
@@ -20,7 +22,15 @@ static inline void pack_gate(gate_desc *gate, unsigned type, unsigned long func,
 	gate->offset_high = (u16)(func >> 32);
 }
 
-static inline void write_gdt_entry(struct desc_struct *gdt, int entry, const void *desc, int type)
+#define write_gdt_entry(dt, entry, desc, type)  native_write_gdt_entry(dt, entry, desc, type)
+#define write_idt_entry(dt, entry, g)           native_write_idt_entry(dt, entry, g)
+
+static inline void native_write_idt_entry(gate_desc *idt, int entry, const gate_desc *gate)
+{
+        memcpy(&idt[entry], gate, sizeof(*gate));
+}
+
+static inline void native_write_gdt_entry(struct desc_struct *gdt, int entry, const void *desc, int type)
 {
         unsigned int size;
         if (type == DESC_TSS)
@@ -31,7 +41,7 @@ static inline void write_gdt_entry(struct desc_struct *gdt, int entry, const voi
         memcpy(&gdt[entry], desc, size);
 }
 
-static inline void set_sys_desc(void *d, unsigned long addr, unsigned type, unsigned size)
+static inline void set_sys_descriptor(void *d, unsigned long addr, unsigned type, unsigned size)
 {
         struct sys_desc_struct *desc = d;
         memset(desc, 0, sizeof(*desc));
@@ -45,5 +55,16 @@ static inline void set_sys_desc(void *d, unsigned long addr, unsigned type, unsi
         desc->base2  = (addr >> 24) & 0xFF;
         desc->base3  = (u32)(addr >> 32);
 }
+
+static inline void __set_tss_desc(struct desc_struct *d, unsigned entry, struct x86_hw_tss *addr)
+{
+        tss_desc tss;
+
+        /* TODO: use a variable to limit size of TSS when io bitmap is implemented */
+        set_sys_descriptor(&tss, (unsigned long)addr, DESC_TSS, sizeof(struct x86_hw_tss) - 1);
+        write_gdt_entry(d, entry, &tss, DESC_TSS);
+}
+
+#define set_tss_desc(dt, addr) __set_tss_desc(dt, GDT_ENTRY_TSS, addr)
 
 #endif // X86_64_DESC_H
