a58aa74e0b7427f2c4b1e2e181bdf6deca9a19fa
Josh F <jjjoshua287@gmail.com>
2026-07-19 18:37:08 -0400

    refactor: rename ISR macros and update exception handling to use consistent naming
    - remove unused header in <asm/idtentry.h>

--- arch/x86_64/include/asm/exceptions.h
+++ arch/x86_64/include/asm/exceptions.h
@@ -5,29 +5,32 @@
  * Vector Numbers corresponding to CPU Exceptions/Interrupts 
  * Vectors 15 and 22-27 are reserved
  */
-#define EXCEPT_DE     0   /* Division Error */
-#define EXCEPT_DB     1   /* Debug */
-#define EXCEPT_NMI    2   /* Non-Maskable Interrupt */
-#define EXCEPT_BP     3   /* Breakpoint */
-#define EXCEPT_OF     4   /* Overflow */
-#define EXCEPT_BR     5   /* Bound range exceeded */
-#define EXCEPT_UD     6   /* Invalid Opcode */
-#define EXCEPT_NM     7   /* Device not available */
-#define EXCEPT_DF     8   /* Double Fault */
-#define EXCEPT_NM     9   /* Coprocessor Segment Overrun (Deprecated in modern CPUs) */
-#define EXCEPT_TS     10  /* Invalid TSS */
-#define EXCEPT_NP     11  /* Segment not present */
-#define EXCEPT_SS     12  /* Stack-Segment Fault */
-#define EXCEPT_GP     13  /* General Protection Fault */
-#define EXCEPT_PF     14  /* Page Fault */
-#define EXCEPT_MF     16  /* x87 Floating Point Exception */
-#define EXCEPT_AC     17  /* Alignment Check */
-#define EXCEPT_MC     18  /* Machine Check */
-#define EXCEPT_XM     19  /* SIMD Floating-Point Exception */
-#define EXCEPT_VE     20  /* Virtualization Exception */
-#define EXCEPT_CP     21  /* Control Protection Exception */
-#define EXCEPT_HV     28  /* Hypervisor Injection Exception */
-#define EXCEPT_VC     29  /* VMM Communication Exception */
-#define EXCEPT_SX     30  /* Security Exception */
+#define EXC_DE          0   /* Division Error */
+#define EXC_DB          1   /* Debug */
+#define EXC_NMI         2   /* Non-Maskable Interrupt */
+#define EXC_BP          3   /* Breakpoint */
+#define EXC_OF          4   /* Overflow */
+#define EXC_BR          5   /* Bound range exceeded */
+#define EXC_UD          6   /* Invalid Opcode */
+#define EXC_NM          7   /* Device not available */
+#define EXC_DF          8   /* Double Fault */
+#define EXC_NM          9   /* Coprocessor Segment Overrun (Deprecated in modern CPUs) */
+#define EXC_TS          10  /* Invalid TSS */
+#define EXC_NP          11  /* Segment not present */
+#define EXC_SS          12  /* Stack-Segment Fault */
+#define EXC_GP          13  /* General Protection Fault */
+#define EXC_PF          14  /* Page Fault */
+#define EXC_SPURIOUS    15  /* Spurious Interrupt (Intel Reserved) */
+#define EXC_MF          16  /* x87 Floating Point Exception */
+#define EXC_AC          17  /* Alignment Check */
+#define EXC_MC          18  /* Machine Check */
+#define EXC_XM          19  /* SIMD Floating-Point Exception */
+#define EXC_VE          20  /* Virtualization Exception */
+#define EXC_CP          21  /* Control Protection Exception */
+#define EXC_HV          28  /* Hypervisor Injection Exception */
+#define EXC_VC          29  /* VMM Communication Exception */
+#define EXC_SX          30  /* Security Exception */
+
+/* Vectors 22-27 are also Intel Reserved */
 
 #endif
--- arch/x86_64/include/asm/idtentry.h
+++ arch/x86_64/include/asm/idtentry.h
@@ -1,47 +1,54 @@
 #ifndef X86_64_IDT_ENTRY_H
 #define X86_64_IDT_ENTRY_H
 
-#include <asm/segment.h>
-
-/* helper here until IRQs are fully implemented */
 #define DECLARE_IDTENTRY(name) void name(void)
 
 /* IDT Entry prototypes */
 
 /* These have no error codes */
-DECLARE_IDTENTRY(asm_isr_divide_error);
-DECLARE_IDTENTRY(asm_isr_debug);
-DECLARE_IDTENTRY(asm_isr_nmi);
-DECLARE_IDTENTRY(asm_isr_breakpoint);
-DECLARE_IDTENTRY(asm_isr_overflow);
-DECLARE_IDTENTRY(asm_isr_bound_range);
-DECLARE_IDTENTRY(asm_isr_invalid_opcode);
-DECLARE_IDTENTRY(asm_isr_device_not_available);
+DECLARE_IDTENTRY(asm_exc_divide_error);
+DECLARE_IDTENTRY(asm_exc_debug);
+DECLARE_IDTENTRY(asm_exc_nmi);
+DECLARE_IDTENTRY(asm_exc_breakpoint);
+DECLARE_IDTENTRY(asm_exc_overflow);
+DECLARE_IDTENTRY(asm_exc_bound_range);
+DECLARE_IDTENTRY(asm_exc_invalid_opcode);
+DECLARE_IDTENTRY(asm_exc_device_not_available);
 
 /* error code of 0 */
-DECLARE_IDTENTRY(asm_isr_double_fault);
+DECLARE_IDTENTRY(asm_exc_double_fault);
 
 /* fault used in old hardware that has no error code */
-DECLARE_IDTENTRY(asm_isr_coprocessor_segment);
+DECLARE_IDTENTRY(asm_exc_coprocessor_segment);
 
 /* These all have an error code */
-DECLARE_IDTENTRY(asm_isr_invalid_tss);
-DECLARE_IDTENTRY(asm_isr_segment_not_present);
-DECLARE_IDTENTRY(asm_isr_stack_segment);
-DECLARE_IDTENTRY(asm_isr_general_protection);
-DECLARE_IDTENTRY(asm_isr_page_fault);
+DECLARE_IDTENTRY(asm_exc_invalid_tss);
+DECLARE_IDTENTRY(asm_exc_segment_not_present);
+DECLARE_IDTENTRY(asm_exc_stack_segment);
+DECLARE_IDTENTRY(asm_exc_general_protection);
+DECLARE_IDTENTRY(asm_exc_page_fault);
 
 /* Control Protection Fault and Alignment Check have error codes, the rest do not */
-DECLARE_IDTENTRY(asm_isr_x87_fpe);
-DECLARE_IDTENTRY(asm_isr_alignment_check);
-DECLARE_IDTENTRY(asm_isr_machine_check);
-DECLARE_IDTENTRY(asm_isr_simd_fpe);
-DECLARE_IDTENTRY(asm_isr_virtualization);
-DECLARE_IDTENTRY(asm_isr_control_protection);
+DECLARE_IDTENTRY(asm_exc_x87_fpe);
+DECLARE_IDTENTRY(asm_exc_alignment_check);
+DECLARE_IDTENTRY(asm_exc_machine_check);
+DECLARE_IDTENTRY(asm_exc_simd_fpe);
+DECLARE_IDTENTRY(asm_exc_virtualization);
+DECLARE_IDTENTRY(asm_exc_control_protection);
 
 /* other ones */
-DECLARE_IDTENTRY(asm_isr_hypervisor_injection);
-DECLARE_IDTENTRY(asm_isr_vmm_comms_exception);
-DECLARE_IDTENTRY(asm_isr_security_exception);
+DECLARE_IDTENTRY(asm_exc_hypervisor_injection);
+DECLARE_IDTENTRY(asm_exc_vmm_comms_exception);
+DECLARE_IDTENTRY(asm_exc_security_exception);
 
-#endif
+/* Intel Reserved Vectors */
+DECLARE_IDTENTRY(asm_exc_reserved_vector_15);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_22);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_23);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_24);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_25);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_26);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_27);
+DECLARE_IDTENTRY(asm_exc_reserved_vector_31);
+
+#endif // X86_64_IDT_ENTRY_H
