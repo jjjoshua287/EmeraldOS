5241b1910b00e3ad2834f8503eddb2d4319d8aff
Josh F <jjjoshua287@gmail.com>
2026-07-19 20:39:32 -0400

    feat(idt): define default IDT gate descriptor table

--- arch/x86_64/kernel/idt.c
+++ arch/x86_64/kernel/idt.c
@@ -40,6 +40,40 @@ enum {
 
 #define IDT_TABLE_SIZE (IDT_ENTRIES * sizeof(gate_desc))
 
+static const struct idt_data def_idt[] = {
+        INTG(EXC_DE,            asm_exc_division_error),
+        ISTG(EXC_DB,            asm_exc_debug, IST_INDEX_DB),
+        ISTG(EXC_NMI,           asm_exc_nmi, IST_INDEX_NMI),
+        SYSG(EXC_BP,            asm_exc_breakpoint),
+        SYSG(EXC_OF,            asm_exc_overflow),
+        INTG(EXC_BR,            asm_exc_bound_range_exceeded),
+        INTG(EXC_UD,            asm_exc_invalid_opcode),
+        INTG(EXC_NM,            asm_exc_device_not_available),
+        ISTG(EXC_DF,            asm_exc_double_fault, IST_INDEX_DF),
+        INTG(EXC_OLD_MP,        asm_exc_coproc_segment_overrun),
+        INTG(EXC_TS,            asm_exc_invalid_tss),
+        INTG(EXC_NP,            asm_exc_segment_not_present),
+        INTG(EXC_SS,            asm_exc_stack_segment_fault),
+        INTG(EXC_GP,            asm_exc_general_protection_fault),
+        INTG(EXC_PF,            asm_exc_page_fault),
+        INTG(EXC_SPURIOUS,      asm_exc_reserved_vector_15),
+        INTG(EXC_MF,            asm_exc_x87_float_point_exception),
+        INTG(EXC_AC,            asm_exc_alignment_check),
+        ISTG(EXC_MC,            asm_exc_machine_check, IST_INDEX_MC),
+        INTG(EXC_XM,            asm_exc_simd_float_point_exception),
+        INTG(EXC_VE,            asm_exc_virtualization_exception),
+        INTG(EXC_CP,            asm_exc_control_protection_exception),
+        INTG(22,                asm_exc_reserved_vector_22),
+        INTG(23,                asm_exc_reserved_vector_23),
+        INTG(24,                asm_exc_reserved_vector_24),
+        INTG(25,                asm_exc_reserved_vector_25),
+        INTG(26,                asm_exc_reserved_vector_26),
+        INTG(27,                asm_exc_reserved_vector_27),
+        INTG(EXC_HV,            asm_exc_hypervisor_injection_exception),
+        INTG(EXC_VC,            asm_exc_vmm_communication_exception),
+        INTG(EXC_SX,            asm_exc_security_exception),
+};
+
 gate_desc idt_table[IDT_ENTRIES];
 
 struct desc_ptr idt_desc = {
