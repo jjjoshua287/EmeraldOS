70929fc72e2b89b8f00d1772978db6a322c9e5ad
Josh F <jjjoshua287@gmail.com>
2026-08-02 09:42:53 -0400

    feat(acpi): move ACPI types and tables definitions to acpi folder

---
+++ include/acpi/acpi_types.h
@@ -0,0 +1,17 @@
+#ifndef ACPI_TYPES_H
+#define ACPI_TYPES_H
+
+/*
+ * ACPI Specification constatns
+*/
+#define ACPI_NAMESEG_SIZE       4     /* Fixed by ACPI spec */
+
+/* Sizes for ACPI table headers */
+#define ACPI_OEM_ID_SIZE        6
+#define ACPI_OEM_TABLE_ID_SIZE  8
+
+#ifndef ACPI_NONSTRING
+#define ACPI_NONSTRING  /* String with no NUL terminator */
+#endif
+
+#endif
