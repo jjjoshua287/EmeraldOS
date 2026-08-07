3b1c0e6932d795a4bee18edf0046047e33e29fc0
Josh F <jjjoshua287@gmail.com>
2026-08-07 11:47:04 -0400

    add functions for writing to PIC

--- arch/x86_64/kernel/acpi/boot.c
+++ arch/x86_64/kernel/acpi/boot.c
@@ -5,9 +5,9 @@
 #include <emerald/types.h>
 #include <emerald/string.h>
 
-static bool checksum_valid(struct acpi_table_rsdp *rsdp, size_t len)
+static bool checksum_valid(void *ptr, size_t len)
 {
-	u8 *bytes = (u8 *)rsdp;
+	u8 *bytes = (u8 *)ptr;
 	u8 sum = 0;
 
     	for (size_t i = 0; i < len; i++)
@@ -22,4 +22,13 @@ static bool validate_rsdp(struct acpi_table_rsdp *rsdp)
 
 	size_t l = (rsdp->revision >= 2) ? rsdp->length : 20;
 	return checksum_valid(rsdp, l);
+}
+
+#define xsdt_entries(len, hdr) ((len) - sizeof((hdr))) / 8
+
+void acpi_boot_init(struct boot_info *boot)
+{
+        struct acpi_table_rsdp *rsdp = (struct acpi_table_rsdp *)boot->rsdp;
+        if (!validate_rsdp(rsdp))
+                panic("Failure to initalize acpi, RSDP is invalid", NULL);
 }
