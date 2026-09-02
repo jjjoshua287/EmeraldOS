818a7d19b4e2737caec7954f9038159c6c6648f5
Josh F <jjjoshua287@gmail.com>
2026-09-02 12:48:51 -0400

    x86_64: add structure for control registers and inline functions for reading them
    - add __always_inline compiler attribute macro

--- arch/x86_64/include/asm/processor.h
+++ arch/x86_64/include/asm/processor.h
@@ -20,16 +20,59 @@
  * @iomap_base: Offset to I/O permission bitmap
  */
 struct x86_hw_tss {
-    u32 reserved1;
-    u64 sp0;
-    u64 sp1;
-    u64 sp2;
-    u64 reserved2;
-    u64 ist[7];
-    u32 reserved3;
-    u32 reserved4;
-    u16 reserved5;
-    u16 iomap_base;
-} __packed;   
+	u32 reserved1;
+	u64 sp0;
+	u64 sp1;
+	u64 sp2;
+	u64 reserved2;
+	u64 ist[7];
+	u32 reserved3;
+	u32 reserved4;
+	u16 reserved5;
+	u16 iomap_base;
+} __packed;
+
+struct control_regs {
+	unsigned long long cr0;
+	unsigned long long cr2;
+	unsigned long long cr3;
+	unsigned long long cr4;
+	unsigned long long cr8;
+} __packed;
+
+static __always_inline unsigned long long native_read_cr0(void)
+{
+    	unsigned long long val;
+    	__asm__ volatile("mov %%cr0, %0" : "=r"(val));
+    	return val;
+}
+
+static __always_inline unsigned long long native_read_cr2(void)
+{
+    	unsigned long long val;
+    	__asm__ volatile("mov %%cr2, %0" : "=r"(val));
+    	return val;
+}
+
+static __always_inline unsigned long long native_read_cr3(void)
+{
+    	unsigned long long val;
+    	__asm__ volatile("mov %%cr3, %0" : "=r"(val));
+    	return val;
+}
+
+static __always_inline unsigned long long native_read_cr4(void)
+{
+    	unsigned long long val;
+    	__asm__ volatile("mov %%cr4, %0" : "=r"(val));
+    	return val;
+}
+
+static __always_inline unsigned long long native_read_cr8(void)
+{
+    	unsigned long long val;
+    	__asm__ volatile("mov %%cr8, %0" : "=r"(val));
+    	return val;
+}
 
 #endif // X86_64_PROCESSOR_H
--- include/emerald/compiler_types.h
+++ include/emerald/compiler_types.h
@@ -5,7 +5,8 @@
 #define __same_type(a, b) __builtin_types_compatible_p(__typeof__(a), __typeof__(b))
 #endif
 
-#define __packed __attribute((__packed__))
-#define __aligned(x) __attribute((__aligned__(x)))
+#define __packed __attribute__((__packed__))
+#define __aligned(x) __attribute__((__aligned__(x)))
+#define __always_inline inline __attribute__((__always_inline__))
 
 #endif
